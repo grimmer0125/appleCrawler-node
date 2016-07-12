3b049fd84215bf653026dc70966705ccc6c20c9d
Grimmer Kang <grimmer0125@gmail.com>
2016-07-12 21:25:56 +0800

    Fix some coding styles.

--- index.js
+++ index.js
@@ -1,34 +1,33 @@
-"use strict";
+'use strict';
 
 require('newrelic');
-var equal = require('deep-equal');
+const equal = require('deep-equal');
 // var Nightmare = require('nightmare');
-var cheerio = require('cheerio');
-var request = require('request');
+const cheerio = require('cheerio');
+const request = require('request');
 
-var _ = require('lodash');
-var bodyParser = require('body-parser');
-var express = require('express');
+const _ = require('lodash');
+const bodyParser = require('body-parser');
+const express = require('express');
 // var request = require('superagent');
-var LineBot = require('line-bot-sdk');
-var client = LineBot.client({
+const LineBot = require('line-bot-sdk');
+const client = LineBot.client({
   channelID: '1472984528',
   channelSecret: 'ef0e7b0d4396b8a410ee0a04a5bdbf9d',
-  channelMID: 'u21e6de33e562aaa212082702d3957721'
+  channelMID: 'u21e6de33e562aaa212082702d3957721',
 });
 
-var db = require('./lib/db');
+const db = require('./lib/db');
 
-function compareWithOldMacs(newMacs){
-
-  db.getAllappleInfo(macs=>{
-    if(equal(newMacs, macs)){
+function compareWithOldMacs(newMacs) {
+  db.getAllappleInfo(macs => {
+    if (equal(newMacs, macs)) {
       console.log('same macs');
-    }else {
-      console.log('old macs:', macs)
+    } else {
+      console.log('old macs:', macs);
 
-      //to udpate
-      if(macs.length==0){
+      // to udpate
+      if (macs.length === 0) {
         console.log('try to insert apple info');
         db.insertAppleInfo(newMacs);
       } else {
@@ -39,8 +38,8 @@ function compareWithOldMacs(newMacs){
       // for testing for xxx's line
       // sendBackMacInfoWhenAddingFriend('xxxx');
 
-      //broadcast by line id, need to recover when not testing
-      db.getAlluserIDs(userList=>{
+      // broadcast by line id, need to recover when not testing
+      db.getAlluserIDs(userList => {
         // console.log("allusers:",userList)
         broadcastUpdatedInfo(userList, newMacs);
       });
@@ -49,18 +48,17 @@ function compareWithOldMacs(newMacs){
 }
 // console.log('broadcast to:'+'userMid;' + JSON.stringify(nameList));
 
-function summaryInfoFromStoredMacs(linebackHandler, macs ){
-
-  const action = (macList)=>{
-    let summaryStr = `蘋果特價品更新:`+'\n\n';
+function summaryInfoFromStoredMacs(linebackHandler, macs) {
+  const action = (macList) => {
+    let summaryStr = '蘋果特價品更新:' + '\n\n';
 
     const numberOfMacs = macList.length;
-    for (let i =0; i<numberOfMacs; i++){
+    for (let i = 0; i < numberOfMacs; i++) {
       const mac = macList[i];
-      if(i == (numberOfMacs - 1)){
-        summaryStr += `${i+1}. ${mac.specsTitle}. ${mac.price} http://www.apple.com${mac.specsURL}`;
-      }else {
-        summaryStr += `${i+1}. ${mac.specsTitle}. ${mac.price} http://www.apple.com${mac.specsURL}`+'\n\n';
+      if (i === (numberOfMacs - 1)) {
+        summaryStr += `${i + 1}. ${mac.specsTitle}. ${mac.price} http://www.apple.com${mac.specsURL}`;
+      } else {
+        summaryStr += `${i + 1}. ${mac.specsTitle}. ${mac.price} http://www.apple.com${mac.specsURL}` + '\n\n';
       }
     }
     console.log('new summary macs:', summaryStr);
@@ -68,32 +66,32 @@ function summaryInfoFromStoredMacs(linebackHandler, macs ){
     linebackHandler(summaryStr);
   };
 
-  if(macs === null){
-    db.getAllappleInfo(finalMacs=>{
+  if (macs === null) {
+    db.getAllappleInfo(finalMacs => {
       action(finalMacs);
     });
-  }else {
+  } else {
     action(macs);
   }
 }
 
-function broadcastUpdatedInfo(userList, newMacs){
-  summaryInfoFromStoredMacs(macInfoString=>{
-    for (const user of userList){
-      console.log('broadcast to:'+ user.id);
+function broadcastUpdatedInfo(userList, newMacs) {
+  summaryInfoFromStoredMacs(macInfoString => {
+    for (const user of userList) {
+      console.log('broadcast to:' + user.id);
       client.sendText(user.id, macInfoString);
     }
   }, newMacs);
 }
 
-function sendBackMacInfoWhenAddingFriend(userMid){
-  summaryInfoFromStoredMacs(macInfoString=>{
-    console.log('send back to:'+ userMid);
+function sendBackMacInfoWhenAddingFriend(userMid) {
+  summaryInfoFromStoredMacs(macInfoString => {
+    console.log('send back to:' + userMid);
     client.sendText(userMid, macInfoString);
   }, null);
 }
 
-var app = express();
+const app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -104,8 +102,7 @@ app.post('/callback', function (req, res) {
   console.log(req.body.result);
 
   var receives = client.createReceivesFromJSON(req.body);
-  _.each(receives, function(receive){
-
+  _.each(receives, function (receive) {
     const clientMid = receive.getFromMid();
     console.log('remote user:', clientMid);
     // if( midList.indexOf(clientMid) < 0 ){
@@ -120,50 +117,38 @@ app.post('/callback', function (req, res) {
     // }
     // console.log('type:', receive.getResult().content.opType);
 
-    if(receive.isMessage()){
-
-      if(receive.isText()){
-
-        if(receive.getText()==='me'){
+    if (receive.isMessage()) {
+      if (receive.isText()) {
+        if (receive.getText() === 'me') {
           client.getUserProfile(receive.getFromMid())
-            .then(function onResult(res){
-              if(res.status === 200){
+            .then(function onResult(res) {
+              if (res.status === 200) {
                 var contacts = res.body.contacts;
-                if(contacts.length > 0){
+                if (contacts.length > 0) {
                   client.sendText(receive.getFromMid(), 'Hi!, you\'re ' + contacts[0].displayName);
                 }
               }
-            }, function onError(err){
+            }, function onError(err) {
               console.error(err);
             });
         } else {
-          console.log('echo:'+ clientMid + ";content:"+receive.getText());
+          console.log('echo:' + clientMid + ';content:' + receive.getText());
           client.sendText(receive.getFromMid(), receive.getText());
         }
-
-      }else if(receive.isImage()){
-
+      } else if (receive.isImage()) {
         client.sendText(receive.getFromMid(), 'Thanks for the image!');
-
-      }else if(receive.isVideo()){
-
+      } else if (receive.isVideo()) {
         client.sendText(receive.getFromMid(), 'Thanks for the video!');
-
-      }else if(receive.isAudio()){
-
+      } else if (receive.isAudio()) {
         client.sendText(receive.getFromMid(), 'Thanks for the audio!');
-
-      }else if(receive.isLocation()){
-
+      } else if (receive.isLocation()) {
         client.sendLocation(
             receive.getFromMid(),
             receive.getText() + receive.getAddress(),
             receive.getLatitude(),
             receive.getLongitude()
           );
-
-      }else if(receive.isSticker()){
-
+      } else if (receive.isSticker()) {
         // This only works if the BOT account have the same sticker too
         client.sendSticker(
             receive.getFromMid(),
@@ -171,20 +156,15 @@ app.post('/callback', function (req, res) {
             receive.getStkPkgId(),
             receive.getStkVer()
           );
-
-      }else if(receive.isContact()){
-
+      } else if (receive.isContact()) {
         client.sendText(receive.getFromMid(), 'Thanks for the contact');
-
-      }else{
+      } else {
         console.error('found unknown message type');
       }
 
       // Todo: will change here
       sendBackMacInfoWhenAddingFriend(clientMid);
-
-    }else if(receive.isOperation()){
-
+    } else if (receive.isOperation()) {
       console.log('found operation');
 
       // until 20160705, https://developers.line.me/bot-api/api-reference#receiving_operations
@@ -198,18 +178,14 @@ app.post('/callback', function (req, res) {
       // }else {
       //   console.log('is not adding contact');
       // }
-      //4 : add or unblock, 8:block
+      // 4 : add or unblock, 8:block <- use this
       // console.log('type:', receive.getResult().content.opType);
 
       db.insertUserID(clientMid);
       sendBackMacInfoWhenAddingFriend(clientMid);
-
-    }else {
-
+    } else {
       console.error('invalid receive type');
-
     }
-
   });
 
   res.send('ok');
@@ -221,66 +197,63 @@ app.listen(app.get('port'), function () {
 
 grabAppleData();
 
-setInterval(function(){
-  console.log('start');
+setInterval(function () {
+  console.log('timer start');
 
   grabAppleData();
 
-  console.log('end');
-}, 12 * 60* 1000); //15 min
+  console.log('timer end');
+}, 12 * 60 * 1000); // 15 min
 
-process.on('SIGINT', function() {
-  process.exit();
-});
-
-function grabAppleData(){
-
+function grabAppleData() {
   request({
-      method: 'GET',
-      url: 'http://www.apple.com/tw/shop/browse/home/specialdeals/mac'
-  }, function(err, response, body) {
-      if (err) {
-        return console.error(err);
-      }
+    method: 'GET',
+    url: 'http://www.apple.com/tw/shop/browse/home/specialdeals/mac',
+  }, function (err, response, body) {
+    if (err) {
+      return console.error(err);
+    }
 
-      var $ = cheerio.load(body);
+    const $ = cheerio.load(body);
 
-      const newMacs = [];
+    const newMacs = [];
 
-      $('.refurb-list .box-content table').each(function(i, elem) {
+    $('.refurb-list .box-content table').each(function (i, elem) {
+      const firstRow = $(this).find('.product');
 
-        const firstRow = $(this).find('.product');
+      const imageColumn = firstRow.find('.image img');
+      const imageSrc = imageColumn.attr('src');
 
-        const imageColumn = firstRow.find(".image img");
-        const imageSrc = imageColumn.attr('src');
+      const specsTitleColumn = firstRow.find('.specs h3');
+      let specsTitleDesc = specsTitleColumn.text();
+      specsTitleDesc = specsTitleDesc.trim();
 
-        const specsTitleColumn = firstRow.find(".specs h3");
-        let specsTitleDesc = specsTitleColumn.text();
-        specsTitleDesc  = specsTitleDesc.trim();
-
-        const specsURLColumn = firstRow.find(".specs h3 a");
-        let specsURL = specsURLColumn.attr('href');
+      const specsURLColumn = firstRow.find('.specs h3 a');
+      const specsURL = specsURLColumn.attr('href');
         // console.log('url:', specsURL);
         // console.log('title desc:' + specsTitleDesc+";end;");
 
-        const specsTotalColumn = firstRow.find(".specs");
-        let specsTotalDesc = specsTotalColumn.text();
+      const specsTotalColumn = firstRow.find('.specs');
+      const specsTotalDesc = specsTotalColumn.text();
         // console.log('before desc:' + specsTotalDesc +";end;");
-        let specsDetailDesc  = specsTotalDesc.replace(specsTitleDesc,'');
-        specsDetailDesc = specsDetailDesc.trim();
+      let specsDetailDesc = specsTotalDesc.replace(specsTitleDesc, '');
+      specsDetailDesc = specsDetailDesc.trim();
         // console.log('desc detail:'+specsDetailDesc+";end;");
 
-        const purchaseInfoColumn = firstRow.find(".purchase-info .price");
-        let price = purchaseInfoColumn.text();
-        price = price.trim();
+      const purchaseInfoColumn = firstRow.find('.purchase-info .price');
+      let price = purchaseInfoColumn.text();
+      price = price.trim();
 
-        const mac = {imageURL:imageSrc, specsURL, specsTitle: specsTitleDesc , specsDetail:specsDetailDesc, price};
+      const mac = { imageURL: imageSrc, specsURL, specsTitle: specsTitleDesc,
+        specsDetail: specsDetailDesc, price };
 
-        newMacs.push(mac);
+      newMacs.push(mac);
+    });
 
-      });
-
-      compareWithOldMacs(newMacs);
-
+    compareWithOldMacs(newMacs);
   });
 }
+
+process.on('SIGINT', function () {
+  process.exit();
+});
